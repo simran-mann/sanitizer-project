1cd51c825abc67e0b73c19bcb61924f6ac3da751
Simran <simranmann2004@gmail.com>
2026-04-07 14:02:22 -0700

    added logic to check for function calls between two array accesses that may modify the pointer, in which case the second check is not redundant and should not be removed

--- llvm-pass/RedundantCheckPass.cpp
+++ llvm-pass/RedundantCheckPass.cpp
@@ -27,6 +27,53 @@ static Value *checkedPointer(const CallInst *CI) {
 }
 
 
+//checks if any instruction between A and B (exclusive) could have invaldated  the pointer being check
+//we could either strilcyt look for free or realloc calls, but this does not cover the case where you call a random function which thne frees the memory 
+//if this function returns true-> dont remove the second access
+static bool isInvalidatedBetween(Instruction *A, Instruction *B,Value *Ptr, DominatorTree &DT) {
+    //go through each instruction, in order and for evry basic block btwn the two instructions, check if any instruction may invalidate the pointer, and if so the second check is needed 
+
+    Function *F = A->getFunction();
+    
+    for (BasicBlock &BB : *F) {
+        for (Instruction &I : BB) {
+            if (&I == A || &I == B) continue;
+            if (!DT.dominates(A, &I)) continue;  //instructions after A
+            if (!DT.dominates(&I, B)) continue;  //instructions before B 
+
+
+            if (auto *CI = dyn_cast<CallInst>(&I)) {
+                //need to consider the case where tje function call is the custom inserted function, or esle there is a  bug where you will never remove hte secnod check
+                if (Function *Fn = CI->getCalledFunction()){
+                    if (Fn->getName() == "check_access"){
+                        continue;
+                    }
+                }
+
+                //if it passes the ptr as an argument, then assume it modifies the memory and so the second check is actually needed 
+
+                /**
+                 * a[i] = 4;
+                 * check_access(a[i]);
+                 * free(unrelated_ptr);
+                 * check_access(a[i]);
+                 * x=a[i];
+                 * 
+                 * with this example, if youre checking for just free or realloc calls,
+                 *  it would not remove the second check, but the ptr that is being checked was never changed 
+                 */
+                for (auto &Arg : CI->args()){
+                    if (Arg.get() == Ptr){//this way is better than explicilty checking if the function name was 'free' or 'realloc', because you could have a snippet like the one above
+                         return true;
+                    }
+                }
+
+            }
+        }
+    }
+    return false;
+}
+
 
 PreservedAnalyses sanitizer::RedundantCheckPass::run(Function &F,FunctionAnalysisManager &AM) {
     // dominator treeto get the depeendency graphs of each check call
@@ -67,7 +114,7 @@ PreservedAnalyses sanitizer::RedundantCheckPass::run(Function &F,FunctionAnalysi
 
             //if they have the same SSA value AND A dominates B , then B is redundant.
             //NOTE: currently this will not work if there was a free or realloc between the two calls, because the SSA form will be the same, but the memory is invalid
-            if (pA == pB && DT.dominates(checks[i], checks[j])){
+            if (pA == pB && DT.dominates(checks[i], checks[j]) && !isInvalidatedBetween(checks[i], checks[j], pA, DT)){
                 toRemove.insert(checks[j]);
             }
         }
